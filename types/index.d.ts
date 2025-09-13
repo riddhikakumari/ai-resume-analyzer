8f2d1b4183e8c606721c0b92e16cee42fc81641c
Riddhika Kumari <kumari.riddhika@gmail.com>
2025-09-14 02:46:16 +0530

    final commit

---
+++ types/index.d.ts
@@ -0,0 +1,129 @@
+interface Job {
+  title: string;
+  description: string;
+  location: string;
+  requiredSkills: string[];
+}
+
+interface FSItem {
+  id: string;
+  uid: string;
+  name: string;
+  path: string;
+  is_dir: boolean;
+  parent_id: string;
+  parent_uid: string;
+  created: number;
+  modified: number;
+  accessed: number;
+  size: number | null;
+  writable: boolean;
+}
+
+interface PuterUser {
+  uuid: string;
+  username: string;
+}
+
+interface KVItem {
+  key: string;
+  value: string;
+}
+
+interface ChatMessageContent {
+  type: "file" | "text";
+  puter_path?: string;
+  text?: string;
+}
+
+interface ChatMessage {
+  role: "user" | "assistant" | "system";
+  content: string | ChatMessageContent[];
+}
+
+interface PuterChatOptions {
+  model?: string;
+  stream?: boolean;
+  max_tokens?: number;
+  temperature?: number;
+  tools?: {
+    type: "function";
+    function: {
+      name: string;
+      description: string;
+      parameters: { type: string; properties: {} };
+    }[];
+  };
+}
+
+interface AIResponse {
+  index: number;
+  message: {
+    role: string;
+    content: string | any[];
+    refusal: null | string;
+    annotations: any[];
+  };
+  logprobs: null | any;
+  finish_reason: string;
+  usage: {
+    type: string;
+    model: string;
+    amount: number;
+    cost: number;
+  }[];
+  via_ai_chat_service: boolean;
+}
+
+interface Resume {
+  id: string;
+  companyName?: string;
+  jobTitle?: string;
+  jobDescription?: string;
+  imagePath: string;
+  resumePath: string;
+  feedback: Feedback;
+}
+
+interface Feedback {
+  overallScore: number;
+  ATS: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+    }[];
+  };
+  toneAndStyle: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  content: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  structure: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  skills: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+}
